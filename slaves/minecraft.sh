d65761a62d3e974e9a62e63bd1ee35d3bcbbd637
YouniS Bensalah <younis.bensalah@gmail.com>
2019-12-30 01:53:47 +0100

    Backup minecraft container without restarting server Close #1

--- slaves/minecraft.sh
+++ slaves/minecraft.sh
@@ -1,12 +1,26 @@
 function main {
-    d "Stopping Minecraft server..."
-    systemctl stop minecraft.service || { e "Could not stop minecraft server."; exit 1; }
+    #d "Stopping Minecraft server..."
+    #systemctl stop minecraft.service || { e "Could not stop minecraft server."; exit 1; }
 
-    d "Saving the world..."
-    tar cvf "$ARCHIVE" /home/minecraft/world /home/minecraft/*.json /home/minecraft/server.properties || { e "Could not create tarball."; exit 1; }
+    sudo -u minecraft -- screen -p 0 -S mc -X eval 'stuff "say INITIATE BACKUP SEQUENCE..."\\015'
 
-    d "Starting Minecraft server..."
-    systemctl start minecraft.service || { e "Could not start minecraft server."; exit 1; }
+    d "Disable auto save..."
+    sudo -u minecraft -- screen -p 0 -S mc -X eval 'stuff "save-off"\\015'
+
+    d "Save the world..."
+    sudo -u minecraft -- screen -p 0 -S mc -X eval 'stuff "save-all"\\015'
+    sleep 10
+
+    d "Create tarball..."
+    tar cvf "$ARCHIVE" /home/minecraft/world /home/minecraft/*.json /home/minecraft/server.properties || { e "Could not create tarball."; screen -p 0 -S mc -X eval 'stuff "save-on"\\015'; exit 1; }
+
+    d "Enable auto save..."
+    sudo -u minecraft -- screen -p 0 -S mc -X eval 'stuff "save-on"\\015'
+
+    sudo -u minecraft -- screen -p 0 -S mc -X eval 'stuff "say BACKUP SEQUENCE COMPLETE."\\015'
+
+    #d "Starting Minecraft server..."
+    #systemctl start minecraft.service || { e "Could not start minecraft server."; exit 1; }
 }
 
 function d {
