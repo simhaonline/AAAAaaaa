84af9d8425fa6c741d0507b08866ee221426b060
YouniS Bensalah <younis.bensalah@gmail.com>
2019-12-30 13:48:38 +0100

    sleep for 30s before running tar

--- slaves/minecraft.sh
+++ slaves/minecraft.sh
@@ -1,21 +1,18 @@
 function main {
     say "Initiate backup sequence..."
 
-    d "Disable auto save..."
+    d "Disable auto save and save world..."
     cmd "save-off"
-    sleep 2
+    cmd "save-all"
+    sleep 30
+    sync
 
     d "Create tarball..."
-    tar cvf "$ARCHIVE" /home/minecraft/world /home/minecraft/*.json /home/minecraft/server.properties || { e "Could not create tarball."; cmd "save-on"; sleep 2; say "Backup sequence incomplete. Please notify an administrator."; exit 1; }
+    tar cvf "$ARCHIVE" /home/minecraft/world /home/minecraft/*.json /home/minecraft/server.properties || { e "Could not create tarball."; cmd "save-on"; say "Backup sequence incomplete. Please notify an administrator."; exit 1; }
     sync
 
     d "Enable auto save..."
     cmd "save-on"
-    sleep 2
-
-    d "Save world..."
-    cmd "save-all"
-    sleep 2
 
     say "Backup sequence complete."
 }
