729ff3a92981b9b86e11d1017b085569e0ba73af
YouniS <younis.bensalah@gmail.com>
2019-12-31 16:25:26 +0100

    Fix path to log file

--- crons/hourly.sh
+++ crons/hourly.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-(cd /home/neo/backups && /home/neo/.aaaa/aaaa.sh -p hourly minecraft 2>&1 | tee -a /home/neo/backups.log) && for f in $(comm -3 <(ls -1 | grep 'hourly') <(tail -n48 <(ls -1 | grep 'hourly'))); do rm "$f"; done
+(cd /home/neo/backups && /home/neo/.aaaa/aaaa.sh -p hourly minecraft 2>&1 | tee -a /home/neo/backups/backups.log) && for f in $(comm -3 <(ls -1 | grep 'hourly') <(tail -n48 <(ls -1 | grep 'hourly'))); do rm "$f"; done
