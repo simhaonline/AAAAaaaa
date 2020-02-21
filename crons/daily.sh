006120c2ed9d694da97688ad44c109c2ecdc95c1
YouniS Bensalah <younis.bensalah@gmail.com>
2020-02-21 15:13:33 +0100

    Add bukkit backup scripts

--- crons/daily.sh
+++ crons/daily.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-(cd /home/neo/backups && /home/neo/.aaaa/aaaa.sh -p daily static reverseproxy mysql minecraft 2>&1 | tee -a /home/neo/backups/backups.log) && cd /home/neo/backups && for f in $(comm -3 <(ls -1 | grep 'daily') <(tail -n30 <(ls -1 | grep 'daily'))); do rm "$f"; done
+(cd /home/neo/backups && /home/neo/.aaaa/aaaa.sh -p daily static reverseproxy mysql bukkit 2>&1 | tee -a /home/neo/backups/backups.log) && cd /home/neo/backups && for f in $(comm -3 <(ls -1 | grep 'daily') <(tail -n30 <(ls -1 | grep 'daily'))); do rm "$f"; done
