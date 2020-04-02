2655390ea20e57714c8df58cb2db0fe87b86d766
YouniS <younis.bensalah@gmail.com>
2020-04-02 23:45:31 +0200

    Keep only 1 weekly nextcloud backup

--- crons/weekly.sh
+++ crons/weekly.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-(cd /home/neo/backups && /home/neo/.aaaa/aaaa.sh -p weekly nextcloud 2>&1 | tee -a /home/neo/backups/backups.log) && cd /home/neo/backups && for f in $(comm -3 <(ls -1 | grep 'weekly') <(tail -n5 <(ls -1 | grep 'weekly'))); do rm "$f"; done
+(cd /home/neo/backups && /home/neo/.aaaa/aaaa.sh -p weekly nextcloud 2>&1 | tee -a /home/neo/backups/backups.log) && cd /home/neo/backups && for f in $(comm -3 <(ls -1 | grep 'weekly') <(tail -n1 <(ls -1 | grep 'weekly'))); do rm "$f"; done
